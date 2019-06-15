26c2f29163ee1633457d473be994b41e753ec737
Jesse Gibson <JesseTheGibson@gmail.com>
2019-06-15 12:20:46 -0600

    Upgrade to babel 7
    Tests are currently broken, naturally, but everything's compiling
    correctly. Well, it's compiling the test directories, but I'll fix that
    in a moment.

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = function(api) {
+  api.cache(false);
+
+  return {
+    presets: [require('@babel/preset-env')],
+    plugins: [require('@babel/plugin-proposal-class-properties')],
+  };
+};
